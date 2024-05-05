67310252137e2765027a201a687f5d95537f28d8
Saeed Khaled <saeedpop1999@gmail.com>
2024-05-05 23:25:42 +0300

    Last Update at '2024/05/05' 11:25PM

--- app/page.js
+++ app/page.js
@@ -5,28 +5,12 @@ import NavBar from "./components/NavBar";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   // ----------- Mouse Cercle ----------- //
-  const [isInside, setIsInside] = useState(false); // حالة لتتبع ما إذا كانت الدائرة داخل السكشن أو لا
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 }); // حالة لتخزين إحداثيات المؤشر داخل السكشن
-
-  // دالة لتحديث إحداثيات المؤشر وحالة الدائرة عند تحرك المؤشر داخل السكشن
-  const handleMouseMove = (event) => {
-    const { clientX, clientY } = event; // الحصول على إحداثيات المؤشر
-    setMousePosition({ x: clientX, y: clientY });
-
-    // تحديث حالة الدائرة بناءً على موقع المؤشر داخل السكشن
-    setIsInside(
-      clientX >= event.target.offsetLeft &&
-        clientX <= event.target.offsetLeft + event.target.offsetWidth &&
-        clientY >= event.target.offsetTop &&
-        clientY <= event.target.offsetTop + event.target.offsetHeight
-    );
-  };
   useEffect(() => {
     // ----------- Smooth Scroll ----------- //
     const lenis = new Lenis({
@@ -49,12 +33,11 @@ export default function Home() {
         <section className={`${styles.section_1} ${styles.section}`}>
           {/* <div className={styles.cursor}></div> */}
           <video
-            src="/public/heroVideo.mp4"
-            data-src="/public/heroVideo.mp4"
-            data-fullscreen-video="video"
-            loop="true"
-            muted="true"
-            autoplay="true"
+            src="/public/assets/heroVideo.mp4"
+            className={styles.video}
+            loop={true}
+            muted={true}
+            autoPlay={true}
           />
           <div className={styles.texts}>
             <h1>
@@ -67,8 +50,6 @@ export default function Home() {
             <button>Play Real</button>
           </div>
         </section>
-        <section className={`${styles.section_2} ${styles.section}`}></section>
-        <section className={`${styles.section_3} ${styles.section}`}></section>
       </main>
     </>
   );
