d0dcbf2d59a66417a5ddeae11f2d714cd2c77ee8
Saeed Khaled <saeedpop1999@gmail.com>
2024-05-06 18:22:09 +0300

    Last Update at '2024/05/06' 06:22AM

--- app/page.js
+++ app/page.js
@@ -5,12 +5,15 @@ import NavBar from "./components/NavBar";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   // ----------- Mouse Cercle ----------- //
+  const circleRef = useRef(null);
+  const textRef = useRef(null);
+
   useEffect(() => {
     // ----------- Smooth Scroll ----------- //
     const lenis = new Lenis({
@@ -25,20 +28,56 @@ export default function Home() {
     gsap.ticker.add((time) => {
       lenis.raf(time * 1000);
     });
+    // ----------- Mouse Cercle ----------- //
+    const circle = circleRef.current;
+    const text = textRef.current;
+    const circleWidth = circle.offsetWidth;
+    const textWidth = text.offsetWidth;
+    const circleHeight = circle.offsetHeight;
+    const textHeight = text.offsetHeight;
+
+    const onMouseMove = (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      gsap.to(circle, {
+        duration: "auto",
+        x: mouseX - circleWidth / 2,
+        y: mouseY - circleHeight / 2,
+      });
+
+      gsap.to(text, {
+        duration: "auto",
+        x: mouseX - textWidth / 2,
+        y: mouseY - textHeight / 2,
+      });
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+    };
   });
   return (
     <>
       <NavBar />
       <main className={styles.page}>
         <section className={`${styles.section_1} ${styles.section}`}>
-          {/* <div className={styles.cursor}></div> */}
-          <video
-            src="/public/assets/heroVideo.mp4"
-            className={styles.video}
-            loop={true}
-            muted={true}
-            autoPlay={true}
-          />
+          <div className={styles.vid}>
+            <div ref={circleRef} className={styles.cursor}></div>
+            <div ref={textRef} className={styles.textInCursor}>
+              Play Real
+            </div>
+            <button className={styles.buttonPlayReal}></button>
+            <video
+              src="/assets/heroVideo.mp4"
+              className={styles.video}
+              loop={true}
+              muted={true}
+              autoPlay={true}
+            />
+          </div>
           <div className={styles.texts}>
             <h1>
               Syncing fast-moving
@@ -50,6 +89,8 @@ export default function Home() {
             <button>Play Real</button>
           </div>
         </section>
+        <section className={`${styles.section_2} ${styles.section}`}></section>
+        <section className={`${styles.section_3} ${styles.section}`}></section>
       </main>
     </>
   );
